62eebbd7d5e44ddc387402bf474469ad5db7e321
omar-kassar <chapilakassar@example.com>
2025-03-08 16:23:17 +0100

    feat : parking crud

--- src/parking/parking.controller.ts
+++ src/parking/parking.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete, Query } from "@nestjs/common";
+import { ParkingService } from "./parking.service";
+import { CreateParkingDto } from "./dto/create-parking.dto";
+import {UpdateParkingDto } from "./dto/update-parking.dto";
 
-@Controller('parking')
-export class ParkingController {}
+@Controller("parkings")
+export class ParkingController {
+  constructor(private readonly parkingService: ParkingService) {}
+
+  @Post()
+  create(@Body() createParkingDto: CreateParkingDto) {
+    return this.parkingService.createParking(createParkingDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.parkingService.getAllParkings();
+  }
+
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return this.parkingService.getParkingById(Number(id));
+  }
+
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateParkingDto: UpdateParkingDto) {
+    return this.parkingService.updateParking(Number(id), updateParkingDto);
+  }
+
+  @Delete(":id")
+  remove(@Param("id") id: string) {
+    return this.parkingService.deleteParking(Number(id));
+  }
+
+  
+}
--- src/parking/parking.service.ts
+++ src/parking/parking.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
+import { CreateParkingDto } from "./dto/create-parking.dto";
+import {UpdateParkingDto } from "./dto/update-parking.dto";
+import { Status } from "@prisma/client";
 
 @Injectable()
-export class ParkingService {}
+export class ParkingService {
+  constructor(private prisma: PrismaService) {}
+
+  //  1. Créer un parking
+  async createParking(dto: CreateParkingDto) {
+    return this.prisma.parking.create({
+      data: {
+        name: dto.name,
+        status: dto.status || Status.PENDING,
+        serviceId: dto.serviceId,
+      },
+    });
+  }
+
+  //  2. Récupérer tous les parkings
+  async getAllParkings() {
+    return this.prisma.parking.findMany({
+      include: { service: true, locations: true }, // Inclut les services et les locations
+    });
+  }
+
+  //  3. Récupérer un parking par ID
+  async getParkingById(id: number) {
+    return this.prisma.parking.findUnique({
+      where: { id },
+      include: { service: true, locations: true },
+    });
+  }
+
+  //  4. Mettre à jour un parking
+  async updateParking(id: number, dto: UpdateParkingDto) {
+    return this.prisma.parking.update({
+      where: { id },
+      data: { name: dto.name, status: dto.status },
+    });
+  }
+
+  //  5. Supprimer un parking
+  async deleteParking(id: number) {
+    return this.prisma.parking.delete({
+      where: { id },
+    });
+  }
+
+  
+  
+  
+}
